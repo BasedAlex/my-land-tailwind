7cdceebd459d49ce58ddbbfc8baf8b2bfe53103c
Based <alexbroseph@mail.ru>
2022-07-21 13:33:06 +0400

    feat-full-validation

--- src/Components/Footer/FooterModal/FooterModal.jsx
+++ src/Components/Footer/FooterModal/FooterModal.jsx
@@ -1,22 +1,57 @@
 import React from 'react'
 import { Formik } from 'formik'
+import * as yup from 'yup'
+
+import InputMask from 'react-input-mask'
 
 function FooterModal() {
+    const unmask = (value) => {
+        const arr = value.split('')
+        return arr
+            .filter((i) => i !== '-' && i !== '(' && i !== ')' && i !== ' ')
+            .join('')
+    }
+
+    const validationSchema = yup.object().shape({
+        name: yup
+            .string()
+            .typeError('Должно быть строкой')
+            .required('Обязательное поле'),
+        phone: yup
+            .string()
+            .typeError('Должно быть строкой')
+            .required('Обязательное поле'),
+        message: yup
+            .string()
+            .typeError('Должно быть строкой')
+            .required('Введите сообщение'),
+    })
+
     return (
         <Formik
             initialValues={{
                 name: '',
                 email: '',
-                phone: '',
                 city: '',
                 message: '',
+                phone: '',
             }}
             onSubmit={(values, { resetForm }) => {
-                console.log(values)
+                const obj = { ...values, phone: unmask(values.phone) }
+                console.log(obj)
                 resetForm({ values: '' })
             }}
+            validationSchema={validationSchema}
         >
-            {({ values, handleSubmit, handleChange }) => (
+            {({
+                values,
+                errors,
+                touched,
+                handleSubmit,
+                handleChange,
+                isValid,
+                dirty,
+            }) => (
                 <div>
                     <p>
                         <label htmlFor={'name'}>Имя</label>
@@ -29,7 +64,9 @@ function FooterModal() {
                             value={values.name}
                         />
                     </p>
-
+                    {touched.name && errors.name && (
+                        <p className="text-red-600">{errors.name}</p>
+                    )}
                     <p>
                         <label htmlFor={'email'}>Email</label>
                         <br />
@@ -45,15 +82,21 @@ function FooterModal() {
                     <p>
                         <label htmlFor={'phone'}>Номер телефона</label>
                         <br />
-                        <input
+                        <InputMask
                             className="px-4"
                             type={'text'}
                             name={'phone'}
                             onChange={handleChange}
                             value={values.phone}
+                            mask="+7(999) 999 99 99"
+                            placeholder="+7(000) 000 00 00"
+                            maskChar=" "
+                            inputMode="numeric"
                         />
                     </p>
-
+                    {touched.phone && errors.phone && (
+                        <p className="text-red-600">{errors.phone}</p>
+                    )}
                     <p>
                         <label htmlFor={'city'}>Ваш город</label>
                         <br />
@@ -78,8 +121,15 @@ function FooterModal() {
                             placeholder={'Введите Ваше сообщение'}
                         />
                     </p>
+                    {touched.message && errors.message && (
+                        <p className="text-red-600">{errors.message}</p>
+                    )}
 
-                    <button onClick={handleSubmit} type={'submit'}>
+                    <button
+                        disabled={!isValid && !dirty}
+                        onClick={handleSubmit}
+                        type={'submit'}
+                    >
                         Отправить
                     </button>
                 </div>
